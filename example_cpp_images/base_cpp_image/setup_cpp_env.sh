43f2dd845b2a2eac87ea3b5d995c3b20b1054afe
miv51 <128756546+miv51@users.noreply.github.com>
2025-07-22 12:07:15 -0400

    Delete example_cpp_images/base_cpp_image/setup_cpp_env.sh

--- example_cpp_images/base_cpp_image/setup_cpp_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script is written for Alpine not Ubuntu
-
-# Install build tools (compilers, etc...), c++ compiler, cmake, and make
-apk add --no-cache g++=14.1.0-r5 cmake=3.29.3-r0 make=4.4.1-r0 musl-dev=1.2.5-r0 linux-headers=6.6.35-r0
