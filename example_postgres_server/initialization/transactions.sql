5f1eb094f52434e931cc077cebaa8b4a1e9dbe8f
miv51 <128756546+miv51@users.noreply.github.com>
2025-09-05 21:31:03 -0400

    Update transactions.sql

--- example_postgres_server/initialization/transactions.sql
+++ example_postgres_server/initialization/transactions.sql
@@ -2,6 +2,11 @@
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY, -- Auto-incrementing unique ID, good practice for any table
     usd_spent INTEGER NOT NULL CHECK (usd_spent >= 0), -- Non-negative integers
-    category VARCHAR(256) NOT NULL, -- Up to 256 Unicode characters
+    category INTEGER NOT NULL CHECK (category >= 0), -- Non-negative integers
     transaction_date DATE NOT NULL -- Stores date, allows month/year extraction
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY, -- Auto-incrementing unique ID, good practice for any table
+    name VARCHAR(256) NOT NULL UNIQUE -- Up to 256 Unicode characters
 );
